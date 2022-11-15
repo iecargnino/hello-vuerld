9fc49f697f7d2110923fa2924f796b4eeb4c0433
Cargnino Emanuel <iecargnino@gmail.com>
2022-11-15 20:09:56 -0300

    testing componentes y modulos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <h1>Listado de Posts:</h1>
   <PostContainer />
+  <br>
+  <CounterOption />
 </template>
 
 <script lang="ts" setup>
   import PostContainer from './components/PostContainer.vue';
+  import CounterOption from './components/CounterOption.vue';
 </script>
 
 <style>
---
+++ src/components/CounterOption.vue
@@ -0,0 +1,32 @@
+<template>
+    <h3>{{ counter }}</h3>
+    <div class="counter-div">
+        <button @click="addCounter">+</button>
+        <button @click="restCounter">-</button>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        name: 'CounterOption',
+        data() {
+            let counter = 0;
+            return { counter }
+        },
+        methods: {
+            addCounter() {
+                this.counter++
+            },
+            restCounter() {
+                this.counter--
+            }
+        }
+    })
+</script>
+
+<style scoped>
+    .counter-div, h3 {
+        text-align: center;
+    }
+</style>
