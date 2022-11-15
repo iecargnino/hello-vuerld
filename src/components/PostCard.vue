172772a8bc5f074abb0796d2d84c226b797e2059
Cargnino Emanuel <iecargnino@gmail.com>
2022-11-15 19:54:25 -0300

    primer commit

---
+++ src/components/PostCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="post-card">
+        <h3>TITULO</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea consequuntur quam officiis iusto nisi possimus reiciendis, amet aperiam deserunt, ex vel. Tenetur suscipit alias mollitia, iure nostrum autem ab molestias!</p>
+        <PostButton />
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import PostButton from './PostButton.vue';
+</script>
+
+<style scoped>
+    .post-card {
+        border-radius: 15px;
+        padding: 10px;
+        margin: 5px;
+        border: 1ps solid #ccc;
+    }
+</style>
